9827ab56ebee18e074e7c0569f0564dab9f9c1ae
sherif omran <ghazal.sherif@gmail.com>
2024-03-19 01:02:24 +0200

    Create SEO

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     
@@ -10,7 +11,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
@@ -28,3 +29,13 @@ class Post(models.Model):
     # هذا الجزء خاص بعرض البوست بدلا من عرض اسمه ال اي دي سوف يتم عرضه باسمه في لوحة تحكم الادمن
     def __str__(self):
         return self.title
+    
+    # هذه الطريقة تستخدم الكابون لتحسين محركات البحث
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug,
+        ])
+    
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render,  get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
+from django.views.generic import ListView
+# from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, 
+                                slug=post,
+                                publish__year=year, 
+                                publish__month=month, 
+                                publish__day=day
+                            )
     return render(request, 'blog/post/detail.html', {'post': post})
