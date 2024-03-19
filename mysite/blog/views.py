cbb565ba212fd78e81347cfa8449c9fe18fde0f7
sherif omran <ghazal.sherif@gmail.com>
2024-03-19 23:01:35 +0200

    Using Django Forms To Share Posts Via Email 21

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,  get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.views.generic import ListView
-# from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 
@@ -43,3 +43,26 @@ def post_detail(request, year, month, day, post):
                                 publish__day=day
                             )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+# هذه الوظيفة خاصة بالفورم الخاص بارسال التعليق الخاص بالبوست علي الايميل
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url} \n {cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'ghazal.sherif@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+# @require_POST
